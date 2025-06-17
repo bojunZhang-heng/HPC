87c6dcabfb44a11fd82f027a3634a47862868644
heng-924 <1002129979@qq.com>
2025-06-17 15:50:43 +0800

    Project_1D_MPI/include/write_txt.hpp update

---
+++ Project_Heat_Transfer/Project_1D_MPI/include/write_txt.hpp
@@ -0,0 +1,10 @@
+#ifndef WRITE_TXT_HPP
+#define WRITE_TXT_HPP
+
+#include <vector>
+#include <string>
+
+void write_txt(const std::vector<double>& u, double dx, const std::string& filename);
+
+#endif
+
