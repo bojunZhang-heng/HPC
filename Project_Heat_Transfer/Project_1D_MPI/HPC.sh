4af954cfe4bc6a6b5cfebc47ab156eb64d8d83bb
heng-924 <1002129979@qq.com>
2025-06-17 15:49:42 +0800

    Project_1D_MPI/HPC.sh

--- Project_Heat_Transfer/Project_1D_MPI/HPC.sh
+++ Project_Heat_Transfer/Project_1D_MPI/HPC.sh
@@ -13,7 +13,7 @@ if [ "$1" == "fixed_dt" ]; then
     rm -f error_dx.dat
     for N in 8 16 32 64 128; do
         echo "Running with N = $N"
-        ./build/HeatTransfer1D $N 1e-3 fixed_dt >> run_N${N}.log 2>&1
+        ./build/HeatTransfer1D $N 1.0 fixed_dt >> run_N${N}.log 2>&1
     done
     echo "Done. Data saved to error_dx.dat"
 
@@ -21,7 +21,7 @@ elif [ "$1" == "fixed_dx" ]; then
     rm -f error_dt.dat
     for CFL in 0.5 0.32 0.16 0.08 0.04; do
         echo "Running with CFL = $CFL"
-        ./build/HeatTransfer1D 1e-3 $CFL fixed_dx >> run_CFL${CFL}.log 2>&1
+        ./build/HeatTransfer1D 1.0 $CFL fixed_dx >> run_CFL${CFL}.log 2>&1
     done
     echo "Done. Data saved to error_dt.dat"
 
